48be88c36b3eebb535210921f7fe65577d8171e8
Javier Manzo <javier.r.manzo@gmail.com>
2019-03-11 17:57:58 -0300

    Project init

---
+++ RedditTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditTest
+//
+//  Created by Javi on 11/03/2019.
+//  Copyright © 2019 Javier Manzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
